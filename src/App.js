12a31f38525748a8f73c05a2a92d421a37519d93
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-03-05 20:34:17 +0500

    [add] product card added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import { useEffect, useState } from "react";
+import ProductCard from "./components/productCard/ProductCard";
+
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./style.module.scss";
+import { BsCartPlus } from "react-icons/bs";
+
+function ProductCard({ imgUrl, title, price, priceUnit, slug, isNew }) {
+  return (
+    <div className={styles.card}>
+      <div>
+        {isNew && (
+          <span className={styles.new_label}>Новинка &nbsp;&nbsp;&nbsp;</span>
+        )}
+        <div className={styles.cardHead}>
+          <button
+            className={styles.addToCardBtn}
+            onClick={() => console.log("Click")}
+            title="add to card"
+          >
+            <BsCartPlus />
+          </button>
+        </div>
+        <Link to={`/${slug}`}>
+          <div className={styles.cardImgBox}>
+            <img src={imgUrl} alt="title" />
+          </div>
+          <span className={styles.title}>{title}</span>
+        </Link>
+      </div>
+      <div className={styles.cardBottom}>
+        <div className={styles.priceWrap}>
+          <span className={styles.priceNumber}>{price}</span>
+          <span className={styles.priceUnit}>{priceUnit}</span>
+        </div>
+        <button className={styles.oneClickBtn}>One click buy</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
