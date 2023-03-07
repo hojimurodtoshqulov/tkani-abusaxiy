326bebde2d23aaa87baff0b3e16fba50273d241f
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-03-07 18:51:05 +0500

    dynamic searching implemented

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      console.log("will unmount");
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
