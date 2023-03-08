2579390d19dfedbff5ea631ebae2b85f52a3daaf
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-03-08 21:00:34 +0500

    full menu have been updated

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Menu from "../../components/menu/menu";
 import c from "./home.module.scss";
 
 const Home = () => {
-  return <div className={c.home}>Home page</div>;
+	return (
+		<div className={c.home}>
+			<div className={c.header}>
+				<div className={c.menu}>
+					<Menu />
+				</div>
+				<div className={c.slider}></div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
