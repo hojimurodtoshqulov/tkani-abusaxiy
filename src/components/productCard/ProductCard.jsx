ae1ea5c6312b655ef1402488bae0dbbcd6c8a7b5
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-03-10 00:21:57 +0500

    updated

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -3,57 +3,54 @@ import { Link } from "react-router-dom";
 import styles from "./style.module.scss";
 import { BsCartPlus } from "react-icons/bs";
 
-
-
-function ProductCard({ imageUrl, title, price, priceUnit, slug = "", isNew }) {
 function ProductCard({
-  imageUrl,
-  title,
-  madeIn,
-  size,
-  price,
-  slug = "",
-  isNew,
+	imageUrl,
+	title,
+	madeIn,
+	size,
+	price,
+	slug = "",
+	isNew,
 }) {
-  return (
-    <div className={styles.card}>
-      <div>
-        {isNew && (
-          <span className={styles.new_label}>Новинка &nbsp;&nbsp;&nbsp;</span>
-        )}
-        <div className={styles.cardHead}>
-          <button
-            className={styles.addToCardBtn}
-            onClick={() => console.log("Click")}
-            title="add to card"
-          >
-            <BsCartPlus />
-          </button>
-        </div>
-        <Link to={`/${slug}`}>
-          <div className={styles.cardImgBox}>
-            <img src={imageUrl} alt={title} />
-          </div>
-          <span className={styles.title}>{title}</span>
-          <div className={styles.extraInfo}>
-            <span className={styles.extraInfoItem}>
-              <span>width/height:</span> {size}
-            </span>
-            <span className={styles.extraInfoItem}>
-              <span>made in:</span> {madeIn}
-            </span>
-          </div>
-        </Link>
-      </div>
-      <div className={styles.cardBottom}>
-        <div className={styles.priceWrap}>
-          <span className={styles.priceNumber}>{price}</span>
-          <span className={styles.priceUnit}>uzs</span>
-        </div>
-        <button className={styles.oneClickBtn}>One click buy</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.card}>
+			<div>
+				{isNew && (
+					<span className={styles.new_label}>Новинка &nbsp;&nbsp;&nbsp;</span>
+				)}
+				<div className={styles.cardHead}>
+					<button
+						className={styles.addToCardBtn}
+						onClick={() => console.log("Click")}
+						title="add to card"
+					>
+						<BsCartPlus />
+					</button>
+				</div>
+				<Link to={`/${slug}`}>
+					<div className={styles.cardImgBox}>
+						<img src={imageUrl} alt={title} />
+					</div>
+					<span className={styles.title}>{title}</span>
+					<div className={styles.extraInfo}>
+						<span className={styles.extraInfoItem}>
+							<span>width/height:</span> {size}
+						</span>
+						<span className={styles.extraInfoItem}>
+							<span>made in:</span> {madeIn}
+						</span>
+					</div>
+				</Link>
+			</div>
+			<div className={styles.cardBottom}>
+				<div className={styles.priceWrap}>
+					<span className={styles.priceNumber}>{price}</span>
+					<span className={styles.priceUnit}>uzs</span>
+				</div>
+				<button className={styles.oneClickBtn}>One click buy</button>
+			</div>
+		</div>
+	);
 }
-}
-export default ProductCard
+
+export default ProductCard;
