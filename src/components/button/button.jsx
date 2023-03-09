9fd80e42050fda288fd9642dc7f4bdfcc8bc2159
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-03-09 23:29:30 +0500

    created new

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,16 @@
-import styles from "./button.module.scss";
-
-export default function Button({ btnTitle }) {
-    return (
-        <>
-            <button className={styles.button}>
-                {btnTitle}
-            </button>
-        </>
-    )
+import scss from "./button.module.scss";
+import { Link } from "react-router-dom";
+export default function Button({ btnTitle, btnClassName, link }) {
+	return (
+		<Link to={link}>
+			<button
+				className={
+					btnClassName === "buttonDenger" ? (scss.button && scss.buttonDenger) : scss.button
+				}
+			>
+				{/* {console.log(btnClassName)} */}
+				{btnTitle}
+			</button>
+		</Link>
+	);
 }
