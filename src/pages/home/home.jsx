ffbde471f6a53ed5c97147cd2946520fa5fd9071
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-03-12 14:21:04 +0500

    [fix] card height styles changed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,45 +7,45 @@ import Headercarusel from "../../components/slider/slider";
 import c from "./home.module.scss";
 import cardsData from "../../data/cards.json";
 const Home = () => {
-	console.log("db>>>", cardsData.products);
-	return (
-		<div className={c.home}>
-			<div className={c.header}>
-				<div className={c.menu}>
-					<Menu />
-				</div>
-				<div className={c.slider}>
-					<Headercarusel />
-					<Headericon />
-					<Button
-						btnTitle={"bla bla"}
-						btnClassName={"button"}
-						link={"/blabla"}
-					/>
-					<Button
-						btnTitle={"bla bla"}
-						btnClassName={"buttonBuy"}
-						link={"/blabla"}
-					/>
-				</div>
-			</div>
-{/* <Button  btnTitle={"bla bla"} /> */}
-			<div className={c.cards}>
-				{cardsData.products?.map((item, index) => (
-					<div className={c.card}>
-						<ProductCard
-							imageUrl={`https://picsum.photos/400/400?random=${item.id}`}
-							title={item.title}
-							price={item.price}
-							madeIn="China"
-							size="230sm/200sm"
-							isNew
-						/>
-					</div>
-				))}
-				{/* <ProductCard /> */}
+  console.log("db>>>", cardsData.products);
+  return (
+    <div className={c.home}>
+      <div className={c.header}>
+        <div className={c.menu}>
+          <Menu />
+        </div>
+        <div className={c.slider}>
+          <Headercarusel />
+          <Headericon />
+          <Button
+            btnTitle={"bla bla"}
+            btnClassName={"button"}
+            link={"/blabla"}
+          />
+          <Button
+            btnTitle={"bla bla"}
+            btnClassName={"buttonBuy"}
+            link={"/blabla"}
+          />
+        </div>
+      </div>
+      {/* <Button  btnTitle={"bla bla"} /> */}
+      <div className={c.cards}>
+        {cardsData.products?.map((item, index) => (
+        //   <div className={c.card}>
+            <ProductCard
+              imageUrl={`https://picsum.photos/400/400?random=${item.id}`}
+              title={item.title}
+              price={item.price}
+              madeIn="China"
+              size="230sm/200sm"
+              isNew
+            />
+        //   </div>
+        ))}
+        {/* <ProductCard /> */}
 
-				{/* <ProductCard
+        {/* <ProductCard
 					imageUrl="https://picsum.photos/400/400?random=1"
 					title=" Dak ( suv otqazmidi) Tarkibi : 65% paxta 35% poliesterEni: 180cm"
 					price={2323}
@@ -53,13 +53,13 @@ const Home = () => {
 					size="230sm/200sm"
 					isNew
 				/> */}
-			</div>
+      </div>
 
-			<div className={c.carousel}>
-				<Owlcarousel />
-			</div>
-		</div>
-	);
+      <div className={c.carousel}>
+        <Owlcarousel />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
