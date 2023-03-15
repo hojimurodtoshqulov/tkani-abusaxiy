a42c925f476c67c0559e952149bdd629ef66d423
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-03-16 01:52:55 +0500

    categories have been updated

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -1,69 +1,53 @@
-import React from 'react'
+import React from "react";
 import c from "./category.module.scss";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import DataCateg from "../../data/categsData";
 import Img1 from "../../media/png1.png";
 
-
 const Category = ({ imgUrl, desc }) => {
+	return (
+		<div className={c.categoryMain}>
+			<h3 className={c.title}>Категории для геймеров</h3>
+			<div className={c.circlesWrap}>
+				<Link to={"/"} className={c.card}>
+					<div className={c.circle}>
+						<img src={Img1} />
+					</div>
+					<p>Клавиатуры</p>
+				</Link>
+				<Link to={"/"} className={c.card}>
+					<div className={c.circle}>
+						<img src={Img1} />
+					</div>
+					<p>Клавиатуры</p>
+				</Link>
+				<Link to={"/"} className={c.card}>
+					<div className={c.circle}>
+						<img src={Img1} />
+					</div>
+					<p>Клавиатуры</p>
+				</Link>
+				<Link to={"/"} className={c.card}>
+					<div className={c.circle}>
+						<img src={Img1} />
+					</div>
+					<p>Клавиатуры</p>
+				</Link>
+				<Link to={"/"} className={c.card}>
+					<div className={c.circle}>
+						<img src={Img1} />
+					</div>
+					<p>Клавиатуры</p>
+				</Link>
+				<Link to={"/"} className={c.card}>
+					<div className={c.circle}>
+						<img src={Img1} />
+					</div>
+					<p>Клавиатуры</p>
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <div className={c.categoryMain}>
-      <h3 className={c.title}>Категории для геймеров</h3>
-      <div className={c.circlesWrap}>
-        <Link to={'/'}>
-          <div className={c.card}>
-            <div className={c.circle}>
-              <img src={Img1} />
-            </div>
-            <p>Клавиатуры</p>
-          </div>
-        </Link>
-        <Link to={'/'}>
-          <div className={c.card}>
-            <div className={c.circle}>
-              <img src={Img1} />
-            </div>
-            <p>Клавиатуры</p>
-          </div>
-        </Link>
-        <Link to={'/'}>
-          <div className={c.card}>
-            <div className={c.circle}>
-              <img src={Img1} />
-            </div>
-            <p>Клавиатуры</p>
-          </div>
-        </Link>
-        <Link to={'/'}>
-          <div className={c.card}>
-            <div className={c.circle}>
-              <img src={Img1} />
-            </div>
-            <p>Клавиатуры</p>
-          </div>
-        </Link>
-        <Link to={'/'}>
-          <div className={c.card}>
-            <div className={c.circle}>
-              <img src={Img1} />
-            </div>
-            <p>Клавиатуры</p>
-          </div>
-        </Link>
-        <Link to={'/'}>
-          <div className={c.card}>
-            <div className={c.circle}>
-              <img src={Img1} />
-            </div>
-            <p>Клавиатуры</p>
-          </div>
-        </Link>
-
-
-      </div>
-    </div>
-  )
-}
-
-export default Category
+export default Category;
